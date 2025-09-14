f9024dd24fa8c535c629200d040e3406fe4be62d
keshavsharma74 <keshav165p@gmail.com>
2025-09-14 19:32:59 +0530

    Updated frontend URL

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import userRouter from "./routes/user.routes.js";
 
 const app=express();
 app.use(cors({
-  origin: "excel-analytics-platform-2u9i.vercel.app",
+  origin: "https://excel-analytics-platform-2u9i.vercel.app",
   credentials: true
 }));
 app.use(cookieParser());
